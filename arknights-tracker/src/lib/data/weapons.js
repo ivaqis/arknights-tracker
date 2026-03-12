82338db159f6e0691ac15674b2debe199a41b6d8
Иван Лагунов <mak.ivan89@gmail.com>
2026-03-12 14:33:32 +0300

    public release v.1.0.13 tangtang data

--- arknights-tracker/src/lib/data/weapons.js
+++ arknights-tracker/src/lib/data/weapons.js
@@ -7,23 +7,17 @@ export const weapons = {
     "rarity": 4,
     "weapon": "greatSword"
   },
-  "oblivion": {
-    "id": "oblivion",
-    "name": "Oblivion",
-    "rarity": 6,
-    "weapon": "artsUnit"
-  },
   "exemplar": {
     "id": "exemplar",
     "name": "Exemplar",
     "rarity": 6,
     "weapon": "greatSword"
   },
-  "sunderedPrince": {
-    "id": "sunderedPrince",
-    "name": "Sundered Prince",
-    "rarity": 6,
-    "weapon": "greatSword"
+  "longRoad": {
+    "id": "longRoad",
+    "name": "Long Road",
+    "rarity": 4,
+    "weapon": "handcannon"
   },
   "formerFinery": {
     "id": "formerFinery",
@@ -37,10 +31,28 @@ export const weapons = {
     "rarity": 6,
     "weapon": "greatSword"
   },
-  "opusTheLiving": {
-    "id": "opusTheLiving",
-    "name": "Opus: The Living",
-    "rarity": 5,
+  "opusEtchFigure": {
+    "id": "opusEtchFigure",
+    "name": "Opus: Etch Figure",
+    "rarity": 6,
+    "weapon": "artsUnit"
+  },
+  "chivalricVirtues": {
+    "id": "chivalricVirtues",
+    "name": "Chivalric Virtues",
+    "rarity": 6,
+    "weapon": "artsUnit"
+  },
+  "sunderedPrince": {
+    "id": "sunderedPrince",
+    "name": "Sundered Prince",
+    "rarity": 6,
+    "weapon": "greatSword"
+  },
+  "peco5": {
+    "id": "peco5",
+    "name": "Peco 5",
+    "rarity": 3,
     "weapon": "handcannon"
   },
   "quencher": {
@@ -49,23 +61,11 @@ export const weapons = {
     "rarity": 4,
     "weapon": "greatSword"
   },
-  "chivalricVirtues": {
-    "id": "chivalricVirtues",
-    "name": "Chivalric Virtues",
-    "rarity": 6,
-    "weapon": "artsUnit"
-  },
   "darhoff7": {
     "id": "darhoff7",
     "name": "Darhoff 7",
     "rarity": 3,
     "weapon": "greatSword"
-  },
-  "eminentRepute": {
-    "id": "eminentRepute",
-    "name": "Eminent Repute",
-    "rarity": 6,
-    "weapon": "sword"
   },
   "seekerOfDarkLung": {
     "id": "seekerOfDarkLung",
@@ -79,41 +79,11 @@ export const weapons = {
     "rarity": 5,
     "weapon": "greatSword"
   },
-  "aggeloslayer": {
-    "id": "aggeloslayer",
-    "name": "Aggeloslayer",
-    "rarity": 4,
-    "weapon": "polearm"
-  },
-  "rapidAscent": {
-    "id": "rapidAscent",
-    "name": "Rapid Ascent",
-    "rarity": 6,
-    "weapon": "sword"
-  },
   "khravengger": {
     "id": "khravengger",
     "name": "Khravengger",
     "rarity": 6,
     "weapon": "greatSword"
-  },
-  "opusEtchFigure": {
-    "id": "opusEtchFigure",
-    "name": "Opus: Etch Figure",
-    "rarity": 6,
-    "weapon": "artsUnit"
-  },
-  "opero77": {
-    "id": "opero77",
-    "name": "Opero 77",
-    "rarity": 3,
-    "weapon": "polearm"
-  },
-  "umbralTorch": {
-    "id": "umbralTorch",
-    "name": "Umbral Torch",
-    "rarity": 6,
-    "weapon": "sword"
   },
   "ancientCanal": {
     "id": "ancientCanal",
@@ -121,11 +91,11 @@ export const weapons = {
     "rarity": 5,
     "weapon": "greatSword"
   },
-  "fluorescentRoc": {
-    "id": "fluorescentRoc",
-    "name": "Fluorescent Roc",
+  "howlingGuard": {
+    "id": "howlingGuard",
+    "name": "Howling Guard",
     "rarity": 4,
-    "weapon": "artsUnit"
+    "weapon": "handcannon"
   },
   "objHeavyBurden": {
     "id": "objHeavyBurden",
@@ -133,11 +103,29 @@ export const weapons = {
     "rarity": 5,
     "weapon": "greatSword"
   },
+  "wedge": {
+    "id": "wedge",
+    "name": "Wedge",
+    "rarity": 6,
+    "weapon": "handcannon"
+  },
+  "objVelocitous": {
+    "id": "objVelocitous",
+    "name": "OBJ Velocitous",
+    "rarity": 5,
+    "weapon": "handcannon"
+  },
   "hypernovaAuto": {
     "id": "hypernovaAuto",
     "name": "Hypernova Auto",
     "rarity": 4,
     "weapon": "artsUnit"
+  },
+  "tarr11": {
+    "id": "tarr11",
+    "name": "Tarr 11",
+    "rarity": 3,
+    "weapon": "sword"
   },
   "jiminy12": {
     "id": "jiminy12",
@@ -145,11 +133,11 @@ export const weapons = {
     "rarity": 3,
     "weapon": "artsUnit"
   },
-  "fortmaker": {
-    "id": "fortmaker",
-    "name": "Fortmaker",
-    "rarity": 5,
-    "weapon": "sword"
+  "fluorescentRoc": {
+    "id": "fluorescentRoc",
+    "name": "Fluorescent Roc",
+    "rarity": 4,
+    "weapon": "artsUnit"
   },
   "wildWanderer": {
     "id": "wildWanderer",
@@ -157,11 +145,11 @@ export const weapons = {
     "rarity": 5,
     "weapon": "artsUnit"
   },
-  "twelveQuestions": {
-    "id": "twelveQuestions",
-    "name": "Twelve Questions",
-    "rarity": 5,
-    "weapon": "sword"
+  "pathfindersBeacon": {
+    "id": "pathfindersBeacon",
+    "name": "Pathfinder's Beacon",
+    "rarity": 4,
+    "weapon": "polearm"
   },
   "stanzaOfMemorials": {
     "id": "stanzaOfMemorials",
@@ -169,33 +157,27 @@ export const weapons = {
     "rarity": 5,
     "weapon": "artsUnit"
   },
+  "objRazorhorn": {
+    "id": "objRazorhorn",
+    "name": "OBJ Razorhorn",
+    "rarity": 5,
+    "weapon": "polearm"
+  },
   "monaihe": {
     "id": "monaihe",
     "name": "Monaihe",
     "rarity": 5,
     "weapon": "artsUnit"
   },
-  "contingentMeasure": {
-    "id": "contingentMeasure",
-    "name": "Contingent Measure",
-    "rarity": 4,
-    "weapon": "sword"
-  },
-  "finchaser30": {
-    "id": "finchaser30",
-    "name": "Finchaser 3.0",
-    "rarity": 5,
-    "weapon": "sword"
+  "deliveryGuaranteed": {
+    "id": "deliveryGuaranteed",
+    "name": "Delivery Guaranteed",
+    "rarity": 6,
+    "weapon": "artsUnit"
   },
   "detonationUnit": {
     "id": "detonationUnit",
     "name": "Detonation Unit",
-    "rarity": 6,
-    "weapon": "artsUnit"
-  },
-  "deliveryGuaranteed": {
-    "id": "deliveryGuaranteed",
-    "name": "Delivery Guaranteed",
     "rarity": 6,
     "weapon": "artsUnit"
   },
@@ -205,11 +187,11 @@ export const weapons = {
     "rarity": 5,
     "weapon": "artsUnit"
   },
-  "cohesiveTraction": {
-    "id": "cohesiveTraction",
-    "name": "Cohesive Traction",
-    "rarity": 5,
-    "weapon": "polearm"
+  "oblivion": {
+    "id": "oblivion",
+    "name": "Oblivion",
+    "rarity": 6,
+    "weapon": "artsUnit"
   },
   "dreamsOfTheStarryBeach": {
     "id": "dreamsOfTheStarryBeach",
@@ -223,16 +205,28 @@ export const weapons = {
     "rarity": 5,
     "weapon": "artsUnit"
   },
-  "pathfindersBeacon": {
-    "id": "pathfindersBeacon",
-    "name": "Pathfinder's Beacon",
-    "rarity": 4,
-    "weapon": "polearm"
-  },
   "chimericJustice": {
     "id": "chimericJustice",
     "name": "Chimeric Justice",
     "rarity": 5,
+    "weapon": "polearm"
+  },
+  "cohesiveTraction": {
+    "id": "cohesiveTraction",
+    "name": "Cohesive Traction",
+    "rarity": 5,
+    "weapon": "polearm"
+  },
+  "aggeloslayer": {
+    "id": "aggeloslayer",
+    "name": "Aggeloslayer",
+    "rarity": 4,
+    "weapon": "polearm"
+  },
+  "opero77": {
+    "id": "opero77",
+    "name": "Opero 77",
+    "rarity": 3,
     "weapon": "polearm"
   },
   "valiant": {
@@ -240,6 +234,24 @@ export const weapons = {
     "name": "Valiant",
     "rarity": 6,
     "weapon": "polearm"
+  },
+  "opusTheLiving": {
+    "id": "opusTheLiving",
+    "name": "Opus: The Living",
+    "rarity": 5,
+    "weapon": "handcannon"
+  },
+  "artzyTyrannical": {
+    "id": "artzyTyrannical",
+    "name": "Artzy Tyrannical",
+    "rarity": 6,
+    "weapon": "handcannon"
+  },
+  "twelveQuestions": {
+    "id": "twelveQuestions",
+    "name": "Twelve Questions",
+    "rarity": 5,
+    "weapon": "sword"
   },
   "jet": {
     "id": "jet",
@@ -253,40 +265,9 @@ export const weapons = {
     "rarity": 6,
     "weapon": "polearm"
   },
-  "artzyTyrannical": {
-    "id": "artzyTyrannical",
-    "name": "Artzy Tyrannical",
-    "rarity": 6,
-    "weapon": "handcannon",
-    "isNew": false
-  },
-  "objRazorhorn": {
-    "id": "objRazorhorn",
-    "name": "OBJ Razorhorn",
-    "rarity": 5,
-    "weapon": "polearm"
-  },
-  "peco5": {
-    "id": "peco5",
-    "name": "Peco 5",
-    "rarity": 3,
-    "weapon": "handcannon"
-  },
-  "howlingGuard": {
-    "id": "howlingGuard",
-    "name": "Howling Guard",
-    "rarity": 4,
-    "weapon": "handcannon"
-  },
-  "longRoad": {
-    "id": "longRoad",
-    "name": "Long Road",
-    "rarity": 4,
-    "weapon": "handcannon"
-  },
-  "whiteNightNova": {
-    "id": "whiteNightNova",
-    "name": "White Night Nova",
+  "grandVision": {
+    "id": "grandVision",
+    "name": "Grand Vision",
     "rarity": 6,
     "weapon": "sword"
   },
@@ -302,17 +283,19 @@ export const weapons = {
     "rarity": 6,
     "weapon": "handcannon"
   },
-  "wedge": {
-    "id": "wedge",
-    "name": "Wedge",
+  "homeLonging": {
+    "id": "homeLonging",
+    "name": "Home Longing",
     "rarity": 6,
-    "weapon": "handcannon"
+    "weapon": "handcannon",
+    "isNew": true
   },
-  "forgebornScathe": {
-    "id": "forgebornScathe",
-    "name": "Forgeborn Scathe",
+  "brigandsCalling": {
+    "id": "brigandsCalling",
+    "name": "Brigand's Calling",
     "rarity": 6,
-    "weapon": "sword"
+    "weapon": "handcannon",
+    "isNew": true
   },
   "clannibal": {
     "id": "clannibal",
@@ -320,27 +303,27 @@ export const weapons = {
     "rarity": 6,
     "weapon": "handcannon"
   },
-  "neverRest": {
-    "id": "neverRest",
-    "name": "Never Rest",
+  "thermiteCutter": {
+    "id": "thermiteCutter",
+    "name": "Thermite Cutter",
     "rarity": 6,
-    "weapon": "sword"
-  },
-  "objVelocitous": {
-    "id": "objVelocitous",
-    "name": "OBJ Velocitous",
-    "rarity": 5,
-    "weapon": "handcannon"
-  },
-  "tarr11": {
-    "id": "tarr11",
-    "name": "Tarr 11",
-    "rarity": 3,
     "weapon": "sword"
   },
   "sunderingSteel": {
     "id": "sunderingSteel",
     "name": "Sundering Steel",
+    "rarity": 5,
+    "weapon": "sword"
+  },
+  "forgebornScathe": {
+    "id": "forgebornScathe",
+    "name": "Forgeborn Scathe",
+    "rarity": 6,
+    "weapon": "sword"
+  },
+  "fortmaker": {
+    "id": "fortmaker",
+    "name": "Fortmaker",
     "rarity": 5,
     "weapon": "sword"
   },
@@ -350,9 +333,27 @@ export const weapons = {
     "rarity": 4,
     "weapon": "sword"
   },
-  "thermiteCutter": {
-    "id": "thermiteCutter",
-    "name": "Thermite Cutter",
+  "umbralTorch": {
+    "id": "umbralTorch",
+    "name": "Umbral Torch",
+    "rarity": 6,
+    "weapon": "sword"
+  },
+  "rapidAscent": {
+    "id": "rapidAscent",
+    "name": "Rapid Ascent",
+    "rarity": 6,
+    "weapon": "sword"
+  },
+  "eminentRepute": {
+    "id": "eminentRepute",
+    "name": "Eminent Repute",
+    "rarity": 6,
+    "weapon": "sword"
+  },
+  "whiteNightNova": {
+    "id": "whiteNightNova",
+    "name": "White Night Nova",
     "rarity": 6,
     "weapon": "sword"
   },
@@ -362,30 +363,29 @@ export const weapons = {
     "rarity": 5,
     "weapon": "sword"
   },
+  "neverRest": {
+    "id": "neverRest",
+    "name": "Never Rest",
+    "rarity": 6,
+    "weapon": "sword"
+  },
+  "gloriousMemory": {
+    "id": "gloriousMemory",
+    "name": "Glorious Memory",
+    "rarity": 6,
+    "weapon": "sword",
+    "isNew": true
+  },
   "objEdgeOfLightness": {
     "id": "objEdgeOfLightness",
     "name": "OBJ Edge of Lightness",
     "rarity": 5,
     "weapon": "sword"
   },
-  "grandVision": {
-    "id": "grandVision",
-    "name": "Grand Vision",
-    "rarity": 6,
+  "finchaser30": {
+    "id": "finchaser30",
+    "name": "Finchaser 3.0",
+    "rarity": 5,
     "weapon": "sword"
-  },
-  "brigandsCalling": {
-    "id": "brigandsCalling",
-    "name": "Brigand's Calling",
-    "rarity": 6,
-    "weapon": "handcannon",
-    "isNew": true
-  },
-  "lupineScarlet": {
-    "id": "lupineScarlet",
-    "name": "Lupine Scarlet",
-    "rarity": 6,
-    "weapon": "sword",
-    "isNew": true
   }
 };
