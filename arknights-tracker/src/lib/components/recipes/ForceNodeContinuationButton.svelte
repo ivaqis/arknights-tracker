7ab76ae3f97231dd02c5346ae82cfa7e40732d80
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-08 22:12:44 +0500

    eye icon

--- arknights-tracker/src/lib/components/recipes/ForceNodeContinuationButton.svelte
+++ arknights-tracker/src/lib/components/recipes/ForceNodeContinuationButton.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Icons from "$lib/components/Icons.svelte";
 </script>
 
 <div class="relative rounded-full h-16 w-16 flex items-center justify-center bg-[#383838] group">
@@ -6,6 +7,9 @@
         class="absolute inset-0 border-[2px] border-white rounded-full z-30 pointer-events-none transition-opacity duration-200 opacity-0 group-hover:opacity-100"
     ></div>
 
-    a
+    <Icons
+        name="eyeOpen"
+        class="text-[#FDFDFD] h-[80%] w-[80%]"
+    />
 
 </div>
