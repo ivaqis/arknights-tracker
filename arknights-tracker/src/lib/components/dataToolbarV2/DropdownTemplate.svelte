76d0267ab408dceb3ab4c9314915f2241c692245
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-18 03:11:13 +0500

    add ResetButton

--- arknights-tracker/src/lib/components/dataToolbarV2/DropdownTemplate.svelte
+++ arknights-tracker/src/lib/components/dataToolbarV2/DropdownTemplate.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { t } from "$lib/i18n";
+    import ResetButton from "$lib/components/dataToolbarV2/ResetButton.svelte";
 
     export let showResetButton = false;
 
@@ -23,12 +23,9 @@
 
             <div class="flex justify-end">
 
-                <button
-                    class="text-xs font-bold text-gray-500 mb-2 dark:text-gray-400 border border-gray-300 dark:border-[#444444] rounded-full px-4 py-1.5 hover:text-[#F9B90C] hover:border-[#F9B90C] hover:bg-white hover:dark:bg-[#FFB200]/80 hover:dark:border-[#FFB200] hover:dark:text-[#E0E0E0] transition-all uppercase tracking-wider"
-                    on:click={onResetButton}
-                >
-                    {$t("sort.reset")}
-                </button>
+                <ResetButton
+                    onClick={onResetButton}
+                />
 
             </div>
 
