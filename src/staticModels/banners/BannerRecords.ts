a3b2c349e87eb321a0c1bb3570394af2e6d2c504
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-06-24 07:19:05 +0500

    add getBanner method

--- src/staticModels/banners/BannerRecords.ts
+++ src/staticModels/banners/BannerRecords.ts
@@ -1,4 +1,5 @@
 import { InvalidBannerEntityError } from "@errors/InvalidBannerEntityError";
+import { Banner } from "@models/banners/Banner";
 import { BannerEntity } from "@staticModels/banners/BannerEntity";
 import { BannerEntityValidator } from "@staticModels/banners/BannerEntityValidator";
 import { RecordsModel } from "@staticModels/RecordsModel";
@@ -7,6 +8,16 @@ export class BannerRecords extends RecordsModel<BannerEntity> {
 
     private constructor(list: BannerEntity[]) {
         super(list, (entity) => entity.id);
+    }
+
+    public getBanner(id: string): Banner | null {
+        let entity = this.get(id);
+
+        if (!entity) {
+            return null;
+        }
+
+        return new Banner(entity);
     }
 
     protected isValid(obj: BannerEntity): boolean {
