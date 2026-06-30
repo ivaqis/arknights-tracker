4706c21cec52e6edaab711896e89149249ef950a
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-06-30 20:53:51 +0500

    update database

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,71 +1,39 @@
-import { GeneratedTokensTable } from "@database/tables/GeneratedTokensTable";
-import { GlobalBannerStatsTable } from "@database/tables/GlobalBannerStatsTable";
-import { GlobalBannerTimelinesTable } from "@database/tables/GlobalBannerTimelinesTable";
-import { GlobalItemStatsTable } from "@database/tables/GlobalItemStatsTable";
-import { GlobalPityDistributionsTable } from "@database/tables/GlobalPityDistributionsTable";
-import { ImportErrorsTable } from "@database/tables/ImportErrorsTable";
-import { UserBannerStatsTable } from "@database/tables/UserBannerStatsTable";
-import { UserBannerTypeStatsTable } from "@database/tables/UserBannerTypeStatsTable";
-import { UserCharBannerPullsTable } from "@database/tables/UserCharBannerPullsTable";
-import { UserCharBannerTypePullsTable } from "@database/tables/UserCharBannerTypePullsTable";
-import { UserBannerProfilesTable } from "@database/tables/UserBannerProfilesTable";
-import { UserWeaponBannerPullsTable } from "@database/tables/UserWeaponBannerPullsTable";
+import { ErrorsRepository } from "@database/repositories/ErrorsRepository";
+import { GlobalBannerStatsRepository } from "@database/repositories/GlobalBannerStatsRepository";
+import { UserBannerProfilesRepository } from "@database/repositories/UserBannerProfilesRepository";
+import { UserBannerStatsRepository } from "@database/repositories/UserBannerStatsRepository";
 import { PrismaClient } from "@prisma/client";
 
 export class Database {
     private readonly _prisma: PrismaClient;
 
-    private readonly _userBannerProfilesRepository: UserBannerProfilesTable;
-    private readonly _generatedTokensRepository: GeneratedTokensTable;
-    private readonly _userBannerStatsRepository: UserBannerStatsTable;
-    private readonly _userBannerTypeStatsRepository: UserBannerTypeStatsTable;
-    private readonly _userCharBannerTypePullsRepository: UserCharBannerTypePullsTable;
-    private readonly _userCharBannerPullsRepository: UserCharBannerPullsTable;
-    private readonly _userWeaponBannerPullsRepository: UserWeaponBannerPullsTable;
-    private readonly _globalBannerStatsRepository: GlobalBannerStatsTable;
-    private readonly _globalBannerTimelinesRepository: GlobalBannerTimelinesTable;
-    private readonly _globalPityDistributionsRepository: GlobalPityDistributionsTable;
-    private readonly _globalItemStatsRepository: GlobalItemStatsTable;
-    private readonly _importErrorsRepository: ImportErrorsTable;
+    private readonly _errorsRepository: ErrorsRepository;
+    private readonly _userBannerProfilesRepository: UserBannerProfilesRepository;
+    private readonly _userBannerStatsRepository: UserBannerStatsRepository;
+    private readonly _globalBannerStatsRepository: GlobalBannerStatsRepository;
 
     public constructor(prisma: PrismaClient) {
-        const isValid = Database.isPrismaValid(prisma);
-
-        if (!isValid) {
-            throw new Error("Invalid prisma client");
-        }
-
         this._prisma = prisma;
 
-        this._userBannerProfilesRepository = new UserBannerProfilesTable(prisma);
-        this._generatedTokensRepository = new GeneratedTokensTable(prisma);
-        this._userBannerStatsRepository = new UserBannerStatsTable(prisma);
-        this._userBannerTypeStatsRepository = new UserBannerTypeStatsTable(prisma);
-        this._userCharBannerTypePullsRepository = new UserCharBannerTypePullsTable(prisma);
-        this._userCharBannerPullsRepository = new UserCharBannerPullsTable(prisma);
-        this._userWeaponBannerPullsRepository = new UserWeaponBannerPullsTable(prisma);
-        this._globalBannerStatsRepository = new GlobalBannerStatsTable(prisma);
-        this._globalBannerTimelinesRepository = new GlobalBannerTimelinesTable(prisma);
-        this._globalPityDistributionsRepository = new GlobalPityDistributionsTable(prisma);
-        this._globalItemStatsRepository = new GlobalItemStatsTable(prisma);
-        this._importErrorsRepository = new ImportErrorsTable(prisma);
+        this._errorsRepository = new ErrorsRepository(prisma);
+        this._userBannerProfilesRepository = new UserBannerProfilesRepository(prisma);
+        this._userBannerStatsRepository = new UserBannerStatsRepository(prisma);
+        this._globalBannerStatsRepository = new GlobalBannerStatsRepository(prisma);
     }
 
-    private static isPrismaValid(prisma: PrismaClient): boolean {
-        return Boolean(
-            prisma
-            && prisma.userBannerProfile
-            && prisma.generatedToken
-            && prisma.userBannerStat
-            && prisma.userBannerTypeStat
-            && prisma.userCharBannerPulls
-            && prisma.userCharBannerTypePulls
-            && prisma.userWeaponBannerPulls
-            && prisma.globalBannerStats
-            && prisma.globalBannerTimeline
-            && prisma.globalPityDistribution
-            && prisma.globalItemStats
-            && prisma.importError
-        );
+    public get errors(): ErrorsRepository {
+        return this._errorsRepository;
+    }
+
+    public get userBannerProfiles(): UserBannerProfilesRepository {
+        return this._userBannerProfilesRepository;
+    }
+
+    public get userBannerStats(): UserBannerStatsRepository {
+        return this._userBannerStatsRepository;
+    }
+
+    public get globalBannerStats(): GlobalBannerStatsRepository {
+        return this._globalBannerStatsRepository;
     }
 }
