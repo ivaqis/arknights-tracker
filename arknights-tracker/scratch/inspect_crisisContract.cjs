01177c8bba5bd66c7f534d6701727fbe94b79287
ivawa <mak.ivan89@gmail.com>
2026-07-05 01:48:08 +0300

    wip_13

--- arknights-tracker/scratch/inspect_crisisContract.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('c:/Users/makiv/Desktop/ArknightProject/accountData.json', 'utf8'));
-
-if (data.data && data.data.detail && data.data.detail.crisisContract) {
-    console.log("Found crisisContract:", JSON.stringify(data.data.detail.crisisContract, null, 2).substring(0, 1500));
-}
--- arknights-tracker/scratch/inspect_weapon_terms.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('c:/Users/makiv/Desktop/ArknightProject/accountData.json', 'utf8'));
-
-const chars = data.data.detail.chars;
-for (const char of chars) {
-    if (char.weapon && char.weapon.weaponTerms) {
-        console.log(`Char: ${char.id || char.charData?.name} has weaponTerms:`, char.weapon.weaponTerms);
-    }
-}
