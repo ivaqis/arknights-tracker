aaa088b748691158140111d41280abf1c6a4fa5e
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-07-06 14:25:27 +0500

    new static models & building fix

--- src/staticModels/instances.ts
+++ src/staticModels/instances.ts
@@ -1,8 +1,12 @@
+import banners from "@static/banners.json";
+import charNames from "@static/charNames.json";
+import equipNames from "@static/equipNames.json";
+import weaponNames from "@static/weaponNames.json";
 import { BannerRecords } from "@staticModels/banners/BannerRecords";
-import banners from "@static/banners.json"
-import itemNames from "@static/itemNames.json"
 import { ItemNameRecords } from "@staticModels/itemNames/ItemNameRecords";
 
 export const bannerRecords = new BannerRecords(banners);
 
-export const itemNameRecords = new ItemNameRecords(itemNames);
+export const charNameRecords = new ItemNameRecords(charNames, "CharNameRecords");
+export const weaponNameRecords = new ItemNameRecords(weaponNames, "WeaponNameRecords");
+export const equipNameRecords = new ItemNameRecords(equipNames, "EquipNameRecords");
--- src/staticModels/itemNames/ItemNameRecords.ts
+++ src/staticModels/itemNames/ItemNameRecords.ts
@@ -1,23 +1,76 @@
+import { logger } from "@/logger";
 import { ItemNameEntity } from "@staticModels/itemNames/ItemNameEntity";
 
 export class ItemNameRecords {
-    private readonly _itemId2ItemName = new Map<string, string>;
-    private readonly _itemName2ItemId = new Map<string, string>;
+    private readonly _id2Name = new Map<string, string>();
+    private readonly _name2Id = new Map<string, string>();
 
-    public constructor(list: ItemNameEntity[]) {
-        for (const item of list) {
-            this._itemId2ItemName.set(item.id, item.name);
-            this._itemName2ItemId.set(item.name, item.id);
+    private _name?: string;
+
+    public constructor(list: ItemNameEntity[], name?: string) {
+        this._name = name;
+
+        this.initialize(list);
+    }
+
+    private get id2NameSize() {
+        return this._id2Name.size;
+    }
+
+    private get name2IdSize() {
+        return this._name2Id.size;
+    }
+
+    private get namePrefix() {
+        if (this._name) {
+            return `${this._name}: `;
         }
+
+        return "";
     }
 
-    public getItemName(id: string): string | null {
-        return this._itemId2ItemName.get(id)
-            ?? null;
+    public getName(id: string): string | undefined {
+        return this._id2Name.get(id);
     }
 
-    public getItemId(name: string): string | null {
-        return this._itemName2ItemId.get(name)
-            ?? null;
+    public getId(name: string): string | undefined {
+        return this._name2Id.get(name);
+    }
+
+    protected initialize(list: ItemNameEntity[]) {
+        logger.debug(`${this.namePrefix}Initializing...`);
+
+        this.initMap(
+            this._id2Name,
+            list,
+            item => item.id,
+            item => item.name
+        );
+
+        this.initMap(
+            this._name2Id,
+            list,
+            item => item.name,
+            item => item.id
+        );
+
+        logger.info(`${this.namePrefix}Initializing completed: ${this.id2NameSize} / ${this.name2IdSize}`);
+    }
+
+    private initMap(map: Map<string, string>,
+                    list: ItemNameEntity[],
+                    getKey: (entity: ItemNameEntity) => string,
+                    getValue: (entity: ItemNameEntity) => string
+    ) {
+        for (const item of list) {
+            let key = getKey(item);
+            let value = getValue(item);
+
+            if (map.has(key)) {
+                logger.warn(`${this.namePrefix}Key "${key}" is already in map`);
+            }
+
+            map.set(key, value);
+        }
     }
 }
