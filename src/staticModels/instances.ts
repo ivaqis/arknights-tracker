f4dddf8b5a8c461dce379ff7ffc3d969ba6830fc
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-07-06 22:04:50 +0500

    new static models

--- src/staticModels/instances.ts
+++ src/staticModels/instances.ts
@@ -1,8 +1,10 @@
 import banners from "@static/banners.json";
 import charNames from "@static/charNames.json";
-import equipNames from "@static/equipNames.json";
-import weaponNames from "@static/weaponNames.json";
 import crisisContracts from "@static/crisisContracts.json";
+import equipNames from "@static/equipNames.json";
+import gemPresetNames from "@static/gemPresetNames.json";
+import weaponNames from "@static/weaponNames.json";
+import tacticalItemNames from "@static/tacticalItemNames.json";
 import { BannerRecords } from "@staticModels/banners/BannerRecords";
 import { CrisisContractRecords } from "@staticModels/crisisContracts/CrisisContractRecords";
 import { ItemNameRecords } from "@staticModels/itemNames/ItemNameRecords";
@@ -12,5 +14,7 @@ export const bannerRecords = new BannerRecords(banners);
 export const charNameRecords = new ItemNameRecords(charNames, "CharNameRecords");
 export const weaponNameRecords = new ItemNameRecords(weaponNames, "WeaponNameRecords");
 export const equipNameRecords = new ItemNameRecords(equipNames, "EquipNameRecords");
+export const gemPresetNameRecords = new ItemNameRecords(gemPresetNames, "GemPresetNameRecords");
+export const tacticalItemNameRecords = new ItemNameRecords(tacticalItemNames, "TacticalItemNameRecords");
 
 export const crisisContractRecords = new CrisisContractRecords(crisisContracts);
