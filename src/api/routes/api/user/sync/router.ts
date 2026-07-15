3c8efef925adb01398cb1fa28d1a3d53ee9d6845
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-07-15 21:30:44 +0500

    add missing validator

--- src/api/routes/api/user/sync/router.ts
+++ src/api/routes/api/user/sync/router.ts
@@ -1,7 +1,8 @@
 import { SyncProfile } from "@api/controllers/syncProfile/SyncProfile";
+import { JsonRequestValidator } from "@api/middleware/validators/JsonRequestValidator";
 import { SyncProfileRequestValidator } from "@api/middleware/validators/syncProfile/SyncProfileRequestValidator";
 import { Router } from "express";
 
 export const syncRouter = Router();
 
-syncRouter.post("/", SyncProfileRequestValidator.validate, SyncProfile.post);
+syncRouter.post("/", JsonRequestValidator.isJson, SyncProfileRequestValidator.validate, SyncProfile.post);
