7e0b636d849405a328d67a4ce319eed000675105
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-07-28 01:39:30 +0500

    FireAndNaturalDamageIncrease & CrystAndPulseDamageIncrease icons

--- arknights-tracker/src/lib/components/dataToolbarV2/paramBoxes/EquipSkillParamBox.svelte
+++ arknights-tracker/src/lib/components/dataToolbarV2/paramBoxes/EquipSkillParamBox.svelte
@@ -10,21 +10,71 @@
 
     $: statIconId = paramId.toLowerCase() === "maxhp"
         ? "hp"
-        : paramId.toLowerCase()
+        : paramId.toLowerCase();
+
+    $: firstElemIconId = getFirstElemIconId(paramId);
+    $: secondElemIconId = getSecondElemIconId(paramId);
+
+    function getFirstElemIconId(paramId) {
+        switch (paramId) {
+            case "FireAndNaturalDamageIncrease": return "heat";
+            case "CrystAndPulseDamageIncrease": return "cryo";
+            default: return "";
+        }
+    }
+
+    function getSecondElemIconId(paramId) {
+        switch (paramId) {
+            case "FireAndNaturalDamageIncrease": return "nature";
+            case "CrystAndPulseDamageIncrease": return "electric";
+            default: return "";
+        }
+    }
+
 </script>
 
 <TextParamBoxTemplate styleMode={styleMode}>
 
     <div
         slot="left"
-        class="w-5 h-5 bg-[#2A2A2A] rounded-[4px] flex items-center justify-center pointer-events-none"
         class:hidden={paramId === "NoAttr"}
     >
+        {#if paramId === "FireAndNaturalDamageIncrease" || paramId === "CrystAndPulseDamageIncrease"}
 
-        <Icon
-            name="{statIconId}"
-            class="w-3 h-3 text-white pointer-events-none"
-        />
+            <div class="relative w-5 h-5 rounded-[4px] pointer-events-none overflow-hidden">
+
+                <div class="absolute top-0 left-0 w-2.5 h-5 overflow-hidden">
+
+                    <Icon
+                        name="{firstElemIconId}"
+                        class="absolute w-5 h-5 top-0 left-0 text-white pointer-events-none"
+                    />
+
+                </div>
+
+                <div class="absolute top-0 left-1/2 w-2.5 h-5 overflow-hidden">
+
+                    <Icon
+                        name="{secondElemIconId}"
+                        class="absolute w-5 h-5 top-0 -left-2.5 text-white pointer-events-none"
+                    />
+
+                </div>
+
+            </div>
+
+        {:else}
+
+            <div class="w-5 h-5 bg-[#2A2A2A] rounded-[4px] flex items-center justify-center pointer-events-none">
+
+                <Icon
+                    name="{statIconId}"
+                    class="w-3 h-3 text-white pointer-events-none"
+                />
+
+            </div>
+
+        {/if}
 
     </div>
 
