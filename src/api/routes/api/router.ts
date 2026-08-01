a033c2368110a9e310717455168d6ca4e897548b
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-08-01 17:51:41 +0500

    move e.json

--- src/api/routes/api/router.ts
+++ src/api/routes/api/router.ts
@@ -4,11 +4,9 @@ import { importRouter } from "@api/routes/api/import/router";
 import { leaderboardRouter } from "@api/routes/api/leaderboard/router";
 import { rankingsRouter } from "@api/routes/api/rankings/router";
 import { userRouter } from "@api/routes/api/user/router";
-import e, { Router } from "express";
+import { Router } from "express";
 
 export const apiRouter = Router();
-
-apiRouter.use(e.json({ limit: "10mb" }));
 
 apiRouter.use("/leaderboard", leaderboardRouter);
 apiRouter.use("/user", userRouter);
--- src/api/routes/root.ts
+++ src/api/routes/root.ts
@@ -1,10 +1,11 @@
 import { RequestLogger } from "@api/middleware/RequestLogger";
 import { apiRouter } from "@api/routes/api/router";
 import { uploadsRouter } from "@api/routes/uploads/router";
-import { Router } from "express";
+import e, { Router } from "express";
 
 export const root = Router();
 
+root.use(e.json({ limit: "10mb" }));
 root.use(RequestLogger.handle);
 
 root.use("/api", apiRouter);
