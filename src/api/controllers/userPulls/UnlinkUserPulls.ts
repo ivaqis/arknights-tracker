8194ba34e6af72389dccbb41bff29364488d25e2
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-08-03 01:36:33 +0500

    UnlinkUserPulls

---
+++ src/api/controllers/userPulls/UnlinkUserPulls.ts
@@ -0,0 +1,72 @@
+import { database, firebase } from "@/serviceInstances";
+import { ResponseBody } from "@api/contracts/ResponseBody";
+import { UnlinkUserPullsQuery } from "@api/contracts/userPulls/UnlinkUserPullsQuery";
+import { UnlinkUserPullsResponse } from "@api/contracts/userPulls/UnlinkUserPullsResponse";
+import { Controller } from "@api/controllers/Controller";
+import { Database } from "@database/Database";
+import { FirebaseAuthenticator } from "@services/firebaseAuth/FirebaseAuthenticator";
+import e from "express";
+
+export class UnlinkUserPulls extends Controller<
+    {},
+    UnlinkUserPullsResponse,
+    undefined,
+    UnlinkUserPullsQuery
+> {
+    public readonly name = "UnlinkUserPulls";
+
+    private readonly _database: Database = database;
+    private readonly _firebase: FirebaseAuthenticator = firebase;
+
+    private readonly _firebaseToken: string;
+    private readonly _gameUid: string;
+
+    public constructor(req: e.Request<{}, ResponseBody<UnlinkUserPullsResponse>, undefined, UnlinkUserPullsQuery>, res: e.Response<ResponseBody<UnlinkUserPullsResponse>>) {
+        super(req, res);
+
+        this._firebaseToken = req.query.firebaseToken;
+        this._gameUid = req.query.gameUid;
+    }
+
+    protected async execute(): Promise<void> {
+        const firebaseUid = await this._firebase.getFirebaseUid(this._firebaseToken);
+
+        if (!firebaseUid) {
+            this.status = 401;
+            this.message = "Unauthorized";
+
+            return;
+        }
+
+        const gameProfile = await this._database.gameProfiles.find(this._gameUid);
+
+        if (!gameProfile) {
+            this.status = 403;
+            this.message = "No access";
+
+            return;
+        }
+
+        const profile = await this._database.users.findUser(gameProfile.uid);
+
+        if (!profile || profile.firebaseUid.initValue === null || profile.firebaseUid.initValue !== firebaseUid) {
+            this.status = 403;
+            this.message = "No access";
+
+            return;
+        }
+
+        const currentBannerProfile = gameProfile.bannerProfileId.initValue === null
+            ? null
+            : await this._database.userBannerProfiles.findUserBannerProfile(gameProfile.bannerProfileId.initValue);
+
+        if (currentBannerProfile) {
+            gameProfile.bannerProfileId.value = null;
+            await this._database.gameProfiles.upsert(gameProfile);
+        }
+
+        this.data = {
+            profileId: currentBannerProfile?.publicId ?? null
+        };
+    }
+}
--- src/api/routes/api/v2/user/pulls/router.ts
+++ src/api/routes/api/v2/user/pulls/router.ts
@@ -1,5 +1,6 @@
 import { Controller } from "@api/controllers/Controller";
 import { LinkUserPulls } from "@api/controllers/userPulls/LinkUserPulls";
+import { UnlinkUserPulls } from "@api/controllers/userPulls/UnlinkUserPulls";
 import { RequestValidator } from "@api/middleware/validators/RequestValidator";
 import { LinkUserPullsRequestValidator } from "@api/middleware/validators/userPulls/LinkUserPullsRequestValidator";
 import { UnlinkUserPullsRequestValidator } from "@api/middleware/validators/userPulls/UnlinkUserPullsRequestValidator";
@@ -10,7 +11,8 @@ export const userPullsRouter = Router();
 userPullsRouter.post("/link",
     RequestValidator.with(LinkUserPullsRequestValidator),
     Controller.with(LinkUserPulls)
-); // todo
+);
 userPullsRouter.delete("/unlink",
-    RequestValidator.with(UnlinkUserPullsRequestValidator)
-); // todo
+    RequestValidator.with(UnlinkUserPullsRequestValidator),
+    Controller.with(UnlinkUserPulls)
+);
