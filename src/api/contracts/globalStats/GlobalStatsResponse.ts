8a7a10d082a2d278bd04a67c7ce5b2f6c1ece53d
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-08-11 18:32:08 +0500

    new GlobalStats controller

--- src/api/contracts/globalStats/GlobalStatsResponse.ts
+++ src/api/contracts/globalStats/GlobalStatsResponse.ts
@@ -1,28 +1,6 @@
-import { GlobalItemData } from "@models/globalBannerStats/GlobalItemData.js";
-import { GlobalPityData } from "@models/globalBannerStats/GlobalPityData.js";
-import { GlobalTimelineData } from "@models/globalBannerStats/GlobalTimelineData.js";
+import { GlobalBannerData } from "@models/globalBannerStats/GlobalBannerData.js";
 
 export interface GlobalStatsResponse {
     bannerId: string;
-    stats: {
-        totalUsers: number;
-        totalPulls: number;
-        total6: number;
-        total5: number;
-        total5050: number;
-        totalWon5050: number;
-        totalLimited: number;
-        freePulls: number;
-        free6: number;
-        free5: number;
-        freeWon5050: number;
-        oroberylSpent: number;
-        medianPity6: number;
-        medianPity5: number;
-    };
-    timeline: GlobalTimelineData[];
-    pityDistribution6: GlobalPityData[]; // zero pity is free
-    pityDistribution5: GlobalPityData[]; // zero pity is free
-    items6: GlobalItemData[];
-    items5: GlobalItemData[];
+    stats: GlobalBannerData;
 }
--- src/api/controllers/globalStats/GlobalStats.ts
+++ src/api/controllers/globalStats/GlobalStats.ts
@@ -4,9 +4,7 @@ import { GlobalStatsResponse } from "@api/contracts/globalStats/GlobalStatsRespo
 import { ResponseBody } from "@api/contracts/ResponseBody.js";
 import { Controller } from "@api/controllers/Controller.js";
 import { Database } from "@database/Database.js";
-import { GlobalItemData } from "@models/globalBannerStats/GlobalItemData.js";
-import { GlobalPityData } from "@models/globalBannerStats/GlobalPityData.js";
-import { GlobalTimelineData } from "@models/globalBannerStats/GlobalTimelineData.js";
+import GlobalBannerStatsAggregator from "@models/globalBannerStats/GlobalBannerStatsAggregator.js";
 import e from "express";
 
 export class GlobalStats extends Controller<
@@ -27,158 +25,21 @@ export class GlobalStats extends Controller<
         this._bannerId = req.query.bannerId;
     }
 
-    private static getMedianPity(pityDistribution: GlobalPityData[], itemsCount: number): number {
-        const half = itemsCount / 2;
-
-        let counter = 0;
-
-        for (const item of pityDistribution) {
-            if (item.pity === 0) {
-                continue;
-            }
-
-            counter += item.count;
-
-            if (counter >= half) {
-                return item.pity;
-            }
-        }
-
-        return 0;
-    }
-
     protected async execute(): Promise<void> {
-        const bannerData = await this._database.userBannerStats.getGlobalBannerStats(this._bannerId);
+        const aggregator = new GlobalBannerStatsAggregator(this._database);
 
-        if (bannerData.totalUsers === 0) {
-            this.data = {
-                bannerId: this._bannerId,
-                stats: {
-                    totalUsers: 0,
-                    totalPulls: 0,
-                    total6: 0,
-                    total5: 0,
-                    total5050: 0,
-                    totalWon5050: 0,
-                    totalLimited: 0,
-                    freePulls: 0,
-                    free6: 0,
-                    free5: 0,
-                    freeWon5050: 0,
-                    oroberylSpent: 0,
-                    medianPity6: 0,
-                    medianPity5: 0
-                },
-                timeline: [],
-                pityDistribution6: [],
-                pityDistribution5: [],
-                items6: [],
-                items5: []
-            };
+        const result = await aggregator.getGlobalBannerData(this._bannerId);
+
+        if (result === null) {
+            this.status = 400;
+            this.message = "Invalid banner id";
 
             return;
         }
 
-        const totalPulls = bannerData.freePulls + bannerData.unfreePulls;
-        const totalLimited = bannerData.won5050 + bannerData.total6 - bannerData.total5050;
-
-        const groupedPityDistribution = await this.getGroupedPityDistribution(bannerData.total6, bannerData.total5);
-        const groupedItems = await this.getGroupedItems(bannerData.total6, bannerData.total5);
-        const timeline = await this.getTimeline(totalPulls);
-
-        const pityDistribution6 = groupedPityDistribution.get(6) ?? [];
-        const pityDistribution5 = groupedPityDistribution.get(5) ?? [];
-
         this.data = {
             bannerId: this._bannerId,
-            stats: {
-                totalUsers: bannerData.totalUsers,
-                totalPulls,
-                total6: bannerData.total6,
-                total5: bannerData.total5,
-                total5050: bannerData.total5050,
-                totalWon5050: bannerData.won5050,
-                totalLimited,
-                freePulls: bannerData.freePulls,
-                free6: bannerData.free6,
-                free5: bannerData.free5,
-                freeWon5050: bannerData.freeWin5050,
-                oroberylSpent: bannerData.unfreePulls * 500,
-                medianPity6: GlobalStats.getMedianPity(pityDistribution6, bannerData.total6 - bannerData.free6),
-                medianPity5: GlobalStats.getMedianPity(pityDistribution5, bannerData.total5 - bannerData.free5)
-            },
-            timeline: timeline,
-            pityDistribution6,
-            pityDistribution5,
-            items6: groupedItems.get(6) ?? [],
-            items5: groupedItems.get(5) ?? []
+            stats: result
         };
-    }
-
-    private async getGroupedPityDistribution(total6: number, total5: number): Promise<Map<number, GlobalPityData[]>> {
-        const data = await this._database.globalBannerStats.getPityDistribution(this._bannerId);
-
-        const map = new Map<number, GlobalPityData[]>();
-
-        for (const item of data) {
-            if (item.rarity !== 6 && item.rarity !== 5) {
-                continue;
-            }
-
-            let list = map.get(item.rarity);
-
-            if (!list) {
-                list = [];
-                map.set(item.rarity, list);
-            }
-
-            list.push({
-                pity: item.pity,
-                count: item.count.initValue,
-                rate: item.count.initValue / (item.rarity === 6 ? total6 : total5),
-            });
-        }
-
-        return map;
-    }
-
-    private async getTimeline(totalPulls: number): Promise<GlobalTimelineData[]> {
-        const data = await this._database.globalBannerStats.getBannerTimeline(this._bannerId);
-
-        return data.map(r => {
-            return {
-                date: r.date,
-                totalPulls: r.totalPullsCount.initValue,
-                freePulls: r.freePullsCount.initValue,
-                rate: r.totalPullsCount.initValue / totalPulls
-            } satisfies GlobalTimelineData;
-        });
-    }
-
-    private async getGroupedItems(total6: number, total5: number): Promise<Map<number, GlobalItemData[]>> {
-        const items = await this._database.globalBannerStats.getItemStats(this._bannerId);
-
-        const map = new Map<number, GlobalItemData[]>();
-
-        for (const item of items) {
-            if (item.rarity !== 6 && item.rarity !== 5) {
-                continue;
-            }
-
-            let list = map.get(item.rarity);
-
-            if (!list) {
-                list = [];
-                map.set(item.rarity, list);
-            }
-
-            list.push({
-                itemId: item.itemId,
-                count: item.count.initValue,
-                rate: item.count.initValue / (item.rarity === 6 ? total6 : total5)
-            });
-        }
-
-        return map;
     }
 }
